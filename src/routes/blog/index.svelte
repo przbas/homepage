4afab2a1f2efeae42e379dcd28a0b8083d73bee3
przbas <przemyslaw.baskiewicz@gmail.com>
2021-01-25 15:50:05 +0100

    Zmiana podstrony blog

--- src/routes/blog/index.svelte
+++ src/routes/blog/index.svelte
@@ -1,14 +1,3 @@
-<script context="module">
-	export function preload({ params, query }) {
-		return this.fetch(`blog.json`).then(r => r.json()).then(posts => {
-			return { posts };
-		});
-	}
-</script>
-
-<script>
-	export let posts;
-</script>
 
 <style>
 	ul {
@@ -30,12 +19,4 @@
 
 <h1>Posty</h1>
 
-<ul>
-	{#each posts as post}
-		<!-- we're using the non-standard `rel=prefetch` attribute to
-				tell Sapper to load the data for the page as soon as
-				the user hovers over the link or taps it, instead of
-				waiting for the 'click' event -->
-		<li><a rel='prefetch' href='blog/{post.slug}'>{post.title}</a> <p>{post.date}</p></li>
-	{/each}
-</ul>
+<p>Coś się pewnie tu pojawi.</p>
